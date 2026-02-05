335300c488309d29c0127aefdd426c684ed994f4
chenjiancheng <431909623@qq.com>
2026-02-05 11:24:16 +0800

    feat: 用户消费service、更新用户信息

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Request, Res, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Put, Query, Request, Res, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { ResponseUtil } from 'src/common/utils/response.util';
 import { RegisterDto } from './dto/register.dto';
@@ -6,6 +6,7 @@ import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { LoginDto } from './dto/login.dto';
 import { JwtAuthGuard } from 'src/auth/jwt.auth.guard';
 import { Public } from 'src/auth/public.decorator';
+import { UpdateUserDto } from './dto/update.dto';
 
 @ApiTags('用户')
 @Controller('user')
@@ -40,4 +41,38 @@ export class UserController {
     const userInfo = await this.userService.getUserInfo(userId);
     return ResponseUtil.success(userInfo, '获取用户信息成功');
   }
+
+    @Put('profile')
+  async updateUserProfile(
+    @Request() req: any,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    const { userId } = req.user;
+    const user = await this.userService.updateUser(userId, updateUserDto);
+    return ResponseUtil.success(user, '更新成功');
+  }
+
+    /**
+   * 获取用户消费记录（包括简历押题、专项面试、综合面试）
+   */
+  @Get('consumption-records')
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({
+    summary: '获取用户消费记录',
+    description:
+      '获取用户所有的功能消费记录，包括简历押题、专项面试、综合面试等',
+  })
+  async getUserConsumptionRecords(
+    @Request() req: any,
+    @Query('skip') skip: number = 0,
+    @Query('limit') limit: number = 20,
+  ) {
+    const { userId } = req.user;
+    const result = await this.userService.getUserConsumptionRecords(userId, {
+      skip,
+      limit,
+    });
+    return ResponseUtil.success(result, '获取成功');
+  }
+
 }
